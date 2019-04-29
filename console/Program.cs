5741052b57e158c1fa9ebe774a93fb34ae88e4b2
kaigeyaotou <kaigeyaotou@gmail.com>
2019-04-29 18:54:12 +0800

    init

---
+++ console/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            wirte();
+            Console.ReadKey();
+        }
+
+        static void wirte()
+        {
+            Console.WriteLine("hello world");
+        }
+    }
+}
